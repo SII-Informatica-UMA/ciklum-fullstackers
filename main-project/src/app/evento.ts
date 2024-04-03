0aae44d89a3c6a25280f002d4ed9ee9b954aabf5
Leuko96 <leandromart8888@gmail.com>
2024-04-03 16:41:00 +0200

    Error areglado
    El error principal que se generaba se resolvía importando en la clase formulario-evento.component.ts "FormsModule" y añadiendo el "standalone:true"

--- main-project/src/app/evento.ts
+++ main-project/src/app/evento.ts
@@ -17,6 +17,7 @@ export interface evento {
     id: number;
 }
 */
+
 export interface evento {
   id: number;
   nombre: string;
--- main-project/src/app/formulario-evento/formulario-evento.component.ts
+++ main-project/src/app/formulario-evento/formulario-evento.component.ts
@@ -1,27 +1,12 @@
 import { Component } from '@angular/core';
-import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
-import { evento } from '../evento';
-import { FormsModule } from '@angular/forms';
-
 
 @Component({
   selector: 'app-formulario-evento',
+  standalone: true,
+  imports: [],
   templateUrl: './formulario-evento.component.html',
-  styleUrls: ['./formulario-evento.component.css']
+  styleUrl: './formulario-evento.component.css'
 })
-
 export class FormularioEventoComponent {
-  accion?: "Añadir" | "Editar";
-  contacto: evento = {id: 0, nombre: '', hora: '', lugar: ''};
-  modal: NgbActiveModal;
-  evento: any;
-
-  constructor(modal: NgbActiveModal) {
-    this.modal = modal;
-  }
-
-  guardarEvento(): void {
-    this.modal.close(this.evento);
-  }
 
 }
