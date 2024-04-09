f934e43c9aebc93b9233ed55b1116375a8805683
JOSÉ MARÍA NOGUERAS NAVARRO <jomanone2@gmail.com>
2024-04-09 23:30:23 +0200

    VERSIÓN 4.0

--- src/app/calendario/calendario.component.ts
+++ src/app/calendario/calendario.component.ts
@@ -16,6 +16,7 @@ export class CalendarioComponent {
   calendario : Evento[] = [
     {nombre: 'Evento 1', descripcion: 'Descripción del evento 1', observaciones: 'Observaciones del evento 1', lugar: 'Lugar del evento 1', duracionMinutos: 60, inicio: '2021-01-01T00:00:00', reglaRecurrencia: 'Regla de recurrencia del evento 1', idCliente: 1, id: 1},
   ]
+  horasDisponibles: string[] = ["9:00", "10:00", "11:00", "12:00", "13:00", "14:00"];
 
   constructor(private modalService: NgbModal) { }
 
@@ -23,8 +24,12 @@ export class CalendarioComponent {
     let ref = this.modalService.open(FormularioEventoComponent);
     ref.componentInstance.accion = "Añadir";
     ref.componentInstance.evento = {nombre: '', descripcion: '', observaciones: '', lugar: '', duracionMinutos: 0, inicio: '', reglaRecurrencia: '', idCliente: 0, id: 0};
-    ref.result.then((evento: Evento) => {
-      this.calendario.push(evento);
+    ref.componentInstance.horasDisponibles = this.horasDisponibles;
+
+    ref.result.then((resultado: {evento: Evento, horaSeleccionada: string}) => {
+      this.calendario.push(resultado.evento);
+      this.calendario.sort((a, b) => a.inicio.localeCompare(b.inicio));
+      this.horasDisponibles = this.horasDisponibles.filter(hora => hora !== resultado.horaSeleccionada);
     });
   }
 }
--- src/app/formulario-evento/formulario-evento.component.ts
+++ src/app/formulario-evento/formulario-evento.component.ts
@@ -1,22 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { Evento } from '../calendario/evento.model';
 import { FormsModule } from '@angular/forms';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-formulario-evento',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, NgFor],
   templateUrl: './formulario-evento.component.html',
   styleUrl: './formulario-evento.component.css'
 })
 export class FormularioEventoComponent {
+  @Input() horasDisponibles: string[] = [];
   accion?: "Añadir" | "Editar";
   evento: Evento = {nombre: '', descripcion: '', observaciones: '', lugar: '', duracionMinutos: 0, inicio: '', reglaRecurrencia: '', idCliente: 0, id: 0};
 
   constructor(public modal: NgbActiveModal) { }
 
+  //horasDisponibles: string[] = this.modal.horasDisponibles;
+
   guardarEvento(): void {
-    this.modal.close(this.evento);
+    this.modal.close({evento: this.evento, horaSeleccionada: this.evento.inicio});
   }
 }
